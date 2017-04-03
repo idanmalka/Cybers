ccc70233bac8c1d84382fb43c3757e03789d3ef8
idanm <idanm@codevalue.net>
2017-04-03 12:34:11 +0300

    Added Infrastructure project( still empty) added bootstrapper and shell(unfinished)

---
+++ Cybers/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace Cybers
+{
+    class Bootstrapper: UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = Shell as Window;
+
+            if (Application.Current.MainWindow != null)
+                Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+        }
+    }
+}
