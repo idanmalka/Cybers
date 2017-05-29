2d2c0bcb69fb6f99be9abc351dde236bf0818eba
Gil <gils@neat-tech.com>
2017-05-30 02:44:10 +0300

    wip

--- Cybers/AlgorithmModule/components/AlgorithmViewModel.cs
+++ Cybers/AlgorithmModule/components/AlgorithmViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AlgorithmModule.interfaces;
 using Cybers.Infrustructure.interfaces;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 
@@ -19,10 +20,22 @@ namespace AlgorithmModule.components
             Init, Clustering, Distributing, Finish, End
         }
 
+        public DelegateCommand TestCommand { get; }
+
         public AlgorithmStep AlgStep
         {
             get => _algStep;
             set => SetProperty(ref _algStep, value);
+        }
+
+        public AlgorithmViewModel()
+        {
+            TestCommand = new DelegateCommand(Test);
+        }
+
+        private void Test()
+        {
+            AlgStep = AlgorithmStep.Clustering;
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
