03bb3d4dc268f48e2c4f021f335a797e558fe6e0
Idan Malka <idanm@codevalue.net>
2017-07-11 15:38:07 +0300

    fixed partition creation problem

--- Cybers/Cybers.Infrustructure/models/Partition.cs
+++ Cybers/Cybers.Infrustructure/models/Partition.cs
@@ -16,49 +16,8 @@ namespace Cybers.Infrustructure.models
             
         }
 
-        public Partition(Graph<User> graph)
-        {
-            var clustersTemp = new Dictionary<long, List<User>>();
-            Clusters = new List<Cluster>();
-
-            //parse vertices from graph to dictionary for easy access of <clusterId,list<User>>
-            foreach (var vertex in graph.Vertices)                  
-                if (clustersTemp.ContainsKey(vertex.ClusterId))
-                    clustersTemp[vertex.ClusterId].Add(vertex);
-                else clustersTemp.Add(vertex.ClusterId, new List<User> { vertex });
-
-            //create new clusters according to the dictionary 
-            for (int i = 0; i < clustersTemp.Count; i++)
-            {
-                var cluster = new Cluster
-                {
-                    Id = i,
-                    Verticies = clustersTemp.ElementAt(i).Value
-                };
-                foreach (var clusterVerticy in cluster.Verticies)
-                {
-                    clusterVerticy.ClusterId = i;
-                }
-                Clusters.Add(cluster);
-            }
-
-            //connect neighbor clusters
-            foreach (var vertex in graph.Vertices)
-            {
-                var userNeighbours = graph.AdjacentVertices(vertex);
-                var cluster = Clusters.FirstOrDefault(c => c.Id == vertex.ClusterId);
-                var neighbourClusters = new List<int>();
-
-                foreach (var neighbour in userNeighbours)
-                    if (neighbour.ClusterId != vertex.ClusterId && !neighbourClusters.Contains(neighbour.ClusterId))
-                        neighbourClusters.Add(neighbour.ClusterId);
-
-                if (cluster != null)
-                    cluster.NeighboursIds = neighbourClusters;
-            }
-
-
-        }
+        public Partition(Graph<User> graph): this (graph.Vertices.ToList())
+        { }
 
         public Partition(List<User> vertices)
         {
@@ -90,8 +49,12 @@ namespace Cybers.Infrustructure.models
             foreach (var vertex in vertices)
             {
                 var userNeighbours = vertex.FriendsList;
+                List<int> neighbourClusters;
                 var cluster = Clusters.FirstOrDefault(c => c.Id == vertex.ClusterId);
-                var neighbourClusters = new List<int>();
+
+                if (cluster.NeighboursIds == null)
+                    neighbourClusters = new List<int>();
+                else neighbourClusters = cluster.NeighboursIds;
 
                 foreach (var neighbour in userNeighbours)
                     if (neighbour.ClusterId != vertex.ClusterId && !neighbourClusters.Contains(neighbour.ClusterId))
